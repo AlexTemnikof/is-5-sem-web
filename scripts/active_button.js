f45d114aa93ce0b583299002f35c877b38485684
Alexei <alexei.temnikov02@gmail.com>
2022-11-05 19:21:54 +0300

    feat: made some design updates

--- scripts/active_button.js
+++ scripts/active_button.js
@@ -6,7 +6,10 @@ window.onload
     if (cur_url === "/resume.html") {
         let button = document.getElementsByClassName("menu-button__home");
         for (let i = 0; i < button.length; i++) {
-            button[i].style.borderLeft = '1px solid black';
+            button[i].style.borderLeft = '3px solid black';
+            button[i].style.borderBottom = '3px solid black';
+            button[i].style.background = 'grey';
+            button[i].style.color = 'white';
             button[i].disabled = true;
         }
     }
@@ -14,7 +17,10 @@ window.onload
     if (cur_url === "/friends.html") {
         let button = document.getElementsByClassName("menu-button__friends");
         for (let i = 0; i < button.length; i++) {
-            button[i].style.borderLeft = '1px solid black';
+            button[i].style.borderLeft = '3px solid black';
+            button[i].style.borderBottom = '3px solid black';
+            button[i].style.background = 'grey';
+            button[i].style.color = 'white';
             button[i].disabled = true;
         }
     }
@@ -22,7 +28,10 @@ window.onload
     if (cur_url === "/cinema.html") {
         let button = document.getElementsByClassName("menu-button__cinema");
         for (let i = 0; i < button.length; i++) {
-            button[i].style.borderLeft = '1px solid black';
+            button[i].style.borderLeft = '3px solid black';
+            button[i].style.borderBottom = '3px solid black';
+            button[i].style.background = 'grey';
+            button[i].style.color = 'white';
             button[i].disabled = true;
         }
     }
@@ -30,7 +39,10 @@ window.onload
     if (cur_url === "/memes.html") {
         let button = document.getElementsByClassName("menu-button__memes");
         for (let i = 0; i < button.length; i++) {
-            button[i].style.borderLeft = '1px solid black';
+            button[i].style.borderLeft = '3px solid black';
+            button[i].style.borderBottom = '3px solid black';
+            button[i].style.background = 'grey';
+            button[i].style.color = 'white';
             button[i].disabled = true;
         }
     }
