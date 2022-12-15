210002cf706ef6b2325cc5352290077738c9d885
Alexei <alexei.temnikov02@gmail.com>
2022-12-15 17:25:13 +0300

    feat: implemented all backend features

---
+++ scripts/construct.js
@@ -0,0 +1,88 @@
+const todoList = document.getElementsByClassName("constructor__todo")[0];
+const todoInput = document.getElementsByClassName("constructor__form")[0];
+let todoArr = [];
+
+
+todoInput.addEventListener("submit", function (e){
+    e.preventDefault();
+    const formData = new FormData(todoInput);
+    let todo = formData.get("todo");
+    let deadline = formData.get("deadline");
+    if (todo === "" || deadline === ""){
+        alert("placeholder all fields");
+    }
+    else {
+        const todoItem = {
+            todo: todo,
+            deadline: deadline,
+            done: false,
+            id: String.fromCharCode(65 + Math.floor(Math.random() * 26)) + Date.now()
+        };
+        todoArr.push(todoItem);
+        todoInput.reset();
+        store(todoArr);
+    }
+});
+
+function store(todoArr){
+    localStorage.setItem("listTodo", JSON.stringify(todoArr));
+    displayTodoList(todoArr);
+}
+
+function getFromStorage(){
+    const ref = localStorage.getItem("listTodo");
+    if (ref){
+        todoArr = JSON.parse(ref);
+        displayTodoList(todoArr);
+    }
+}
+
+function clearStorage(){
+    localStorage.removeItem("listTodo");
+    todoArr.splice(0, todoArr.length);
+    displayTodoList(todoArr);
+}
+
+function displayTodoList(todoArr){
+    todoList.innerHTML = '';
+    todoArr.forEach(function (todo) {
+        const template = document.querySelector("#template");
+        const item = template.content.cloneNode(true);
+        const item2 = item.querySelector(".todo-list__item");
+        const done = todo.done ? "checked" : null;
+        item2.setAttribute("id", todo.id);
+        item.querySelector('input').setAttribute(done, done);
+        item2.querySelector("span").textContent = todo.todo + " " + todo.deadline;
+        todoList.append(item);
+    })
+}
+
+function deleteTodo(id){
+    todoArr = todoArr.filter(function (todo) {
+        return todo.id !== id;
+    });
+    store(todoArr);
+}
+
+function toggle(id){
+    for (let i = 0; i < todoArr.length; i++){
+        if (todoArr[i].id === id){
+            todoArr[i].done = !todoArr[i].done;
+        }
+    }
+    store(todoArr);
+}
+
+getFromStorage();
+
+todoList.addEventListener('click', (event) => {
+    const isButton = event.target.classList.contains("delete__button");
+    const isCheckBox = event.target.classList.contains("checkbox");
+    if (isButton){
+        deleteTodo(event.target.parentElement.id);
+    }
+
+    if(isCheckBox){
+        toggle(event.target.parentElement.id);
+    }
+})
